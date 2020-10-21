b89d94908a37861fc7394d27c1c42b90588005f4
Val Castanteen <valcastanteen@Vals-Air.fios-router.home>
2020-10-21 17:21:22 -0400

    third commit

--- js/script.js
+++ js/script.js
@@ -1,56 +1,68 @@
+
+const url= 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+
+const $drink=$('#drink');
+
+const $get_drink = $('#get_drink');
+
+$get_drink.on('click', getDrinks);
+
+function getDrinks (event){
+	$.ajax(url)
+	.then (function(data){
+		console.log (data) 
+		drinkList = data;
+        render ()
+	}, function(error){
+		console.log ('error',error)
+	})
+}
+
+function render () {
+	$drink.text('drink name ' + drinkList.drinks[0].strDrink);
+
+}
+/*
+previous code
+
 const get_drink_btn = document.getElementById('get_drink');
 const drink_container = document.getElementById('drink');
 
-get_drink_btn.addEventListener('click', () => {
+get_drink_btn.addEventListener('click', (event) => {
 	fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
 		.then(res => res.json())
 		.then(res => {
 			createDrink(res.drink[0]);
-		})
-		.catch(e => {
-			console.warn(e);
 		});
+});
 
-
-    const createDrink = drink => {
+const createDrink = (drink) => {
 	const ingredients = [];
-
 	// Get all ingredients from the object. Up to 15
 	for (let i = 1; i <= 15; i++) {
-		if (drink[`strIngredient${i}`]) {
-			ingredients.push(
-				`${drink[`strIngredient${i}`]} - ${drink[`strMeasure${i}`]}`
-			);
+		if (meal[`strIngredient${i}`]) {
+			ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`)
 		} else {
-			// Stop if there are no more ingredients
+			// Stop if no more ingredients
 			break;
 		}
 	}
 
 	const newInnerHTML = `
-		<div class="row">
-			<div class="columns five">
-				<img src="${drink.strDrinkThumb}" alt=" Drink Image">
-				${
-					drink.strCategory
-						? `<p><strong>Category:</strong> ${drink.strCategory}</p>`
-						: ''
-				}
-				${drink.strAlcoholic ? `<p><strong>Alcoholic:</strong> ${meal.strAlcoholic}</p>` : ''}
-				${drink.strGlass
-						? `<p><strong>Glass:</strong> ${drink.strGlass}</p>`
-						: ''
-				}
-				<h5>Ingredients:</h5>
-				<ul>
-					${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
-				</ul>
-			</div>
-			<div class="columns seven">
-				<h4>${drink.strDrink}</h4>
-				<p>${drink.strInstructions}</p>
-			</div> :"}
-	`;
+        <div class="row">
+            <div class="columns five">
+                <img src="${drink.strDrinkThumb}" alt="Drink Image">
+                ${drink.strAlcoholic ? `<p><strong>Category:</strong> ${drink.strAlcoholic}</p>` : ''}
+                ${drink.strGlass ? `<p><strong>Area:</strong> ${drink.strGlass}</p>` : ''}
+                <h5>Ingredients:</h5>
+                <ul>
+                    ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+                </ul>
+            </div>
+            <div class="columns seven">
+                <h4>${drink.strdrink}</h4>
+                <p>${drink.strInstructions}</p>
+            </div>`
+}
 
-	drink_container.innerHTML = newInnerHTML;
-});
+*/
